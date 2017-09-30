aa9f6993299adb6a5077d31510c4753835ce3e58
Soya Diaoune <soya@notebear.com>
2017-09-30 03:11:11 -0400

    Added new components
    Contact Search, Header, About Page components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactSearchComponent } from './contactsearch.component';
+import { AboutComponent } from './about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    AppComponent,
+    ContactSearchComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full'
+      },
+      {
+        path: 'main',
+        component: ContactSearchComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HeaderComponent, AppComponent]
 })
+
+
 export class AppModule { }
---
+++ src/app/contactsearch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: 'contactsearch.component.html',
+  styleUrls: ['app.component.css']
+})
+
+export class ContactSearchComponent {
+  
+}
